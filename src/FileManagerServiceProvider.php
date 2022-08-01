3387da910b942b542f6d4c1fe788f578a43f2594
Tirdad Abbasi <abbasi.tirdad@gmail.com>
2022-08-01 18:41:19 +0300

    added permissions

--- src/FileManagerServiceProvider.php
+++ src/FileManagerServiceProvider.php
@@ -2,6 +2,8 @@
 namespace Tir\FileManager;
 
 use Illuminate\Support\ServiceProvider;
+use Tir\Crud\Support\Module\Module;
+use Tir\Crud\Support\Module\Modules;
 
 class FileManagerServiceProvider extends ServiceProvider
 {
@@ -25,5 +27,27 @@ class FileManagerServiceProvider extends ServiceProvider
     {
         $this->loadMigrationsFrom(__DIR__ . '/Database/Migrations');
         $this->loadRoutesFrom(__DIR__ . '/Routes/admin.php');
+
+        $fileManager = new Module();
+        $fileManager->setName('file-manager')->enable();
+        $fileManager->setPermissions([
+            [
+                'page' => [
+                    'label' => trans('core::panel.upload'),
+                    'value' => 'upload'
+                ],
+                'access' => [
+                    [
+                        'label' => trans('core::panel.allow'),
+                        'value' => 'allow',
+                    ],
+                    [
+                        'label' => trans('core::panel.deny'),
+                        'value' => 'deny',
+                    ]
+                ]
+            ]
+        ]);
+        Modules::init()->register($fileManager);
     }
 }
