914d209a9802557a8b8dffe13f05632ddac83640
Tirdad Abbasi <abbasi.tirdad@gmail.com>
2023-08-21 16:08:41 +0200

    Update admin.php

--- src/Routes/admin.php
+++ src/Routes/admin.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use Tir\FileManager\Controllers\AdminFileManagerController;
 
 
-Route::group(['middleware' => 'auth:api', 'prefix' => 'api/v1/admin'], function () {
+Route::group(['middleware' => config('crud.middlewares'), 'prefix' => 'api/v1/admin'], function () {
     Route::post('/file-manager/upload',[AdminFileManagerController::class ,'upload'])->name('admin.file-manager.upload');
     Route::resource('/file-manager', AdminFileManagerController::class, ['names' => 'admin.file-manager']);
 });
